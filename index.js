a906e8b89d8c3157cf5ccc6d9b2eba98b4aa19e3
BUSRA KUCUK <busra3590@gmail.com>
2023-05-23 22:55:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let length = word.length;
+  for (let i = 0; i<length / 2; i++){
+    if(word[i] !== word[length - 1 - i]){
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  for i = 0 to length/2;
+  if word[i] is not equal to word[length -1 - i];
+  return false // characters don't match, not a palindrome
+
+  return true // all characters match, it is a palindrome
 */
 
 /*
   Add written explanation of your solution here
+  The 'isPalindrome' function checks whether a given string is a palindrome.
+  It iterates over the characters of the string using 'for' loop, comparing characters
+  from the beginning and end of the string.
+  if any characters match, the function returns 'false', which means the
+  word is not a palindrome.
+  if all characters match, the function returns 'true', indicating that
+  the word is a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
